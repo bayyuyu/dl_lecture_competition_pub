8984799338d2addb277326700538209fa75aefb5
Yuuki TOYODA <64355293+bayyuyu@users.noreply.github.com>
2024-07-17 14:43:40 +0900

    Add files via upload

--- main_240716.py
+++ main_240716.py
@@ -173,7 +173,7 @@ class VQADataset(torch.utils.data.Dataset):
             return image, input_ids, attention_mask
     
     def __len__(self):
-        return len(self.df)
+        return len(self.question2idx)
     
     def load_image(self, idx):
         # Load the image using PIL (same as before)
